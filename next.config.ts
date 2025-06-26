e68e50ca830fee2691dcfe96dad510c033708172
anoopa413 <anoopa413@gmail.com>
2025-06-26 20:42:01 +0530

    Update next.config.ts to uncomment type import and clarify configuration options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// next.config.ts
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+}
 
 export default nextConfig;
